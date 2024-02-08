737ac7874d44817ae002f4d7bc9af532d3aef364
Santiago Alvarez Peña <alvarezsantiago15623@gmail.com>
2024-02-08 11:09:44 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Santiago Alvarez Peña'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About page</h1>')
